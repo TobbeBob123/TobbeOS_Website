cd431f1fd3503ca5039b4c9072bd6dcdbd12ec6d
Tobias Os Finsbol <tobias.finsbol@gmail.com>
2025-05-13 22:01:43 +0200

    centered text

--- TobbeOS.js
+++ TobbeOS.js
@@ -52,6 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			overlay.classList.add("hidden");
 		});
 	}
+	let index = 0;
 	let currentSlide = 0;
 	const slides = document.querySelectorAll('.slide');
 	const prev = document.getElementById('prev');
@@ -62,18 +63,16 @@ document.addEventListener("DOMContentLoaded", function () {
 	function showSlide(index) {
 	  slides.forEach((slide, i) => {
 	    slide.style.display = (i === index) ? 'block' : 'none';
-	  });
+	    });
 	}
 	function changeSlide(step) {
 	  currentSlide = (currentSlide + step + slides.length) % slides.length;
 	  showSlide(currentSlide);
 	}
 
-	// Auto-play (optional)
 	setInterval(() => {
 	  changeSlide(1);
-	}, 4000); // 4 seconds
+	}, 10000);
 
-	// Initial display
 	showSlide(currentSlide);
 });
